66ad7200a1b188ad1bcfc93ad2a5d1e6435318df
Thiago Lisandro Porfirio <thiagolisandroporfirio@gmail.com>
2024-04-30 11:39:01 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MyBuilding',
+  webDir: 'www'
+};
+
+export default config;
